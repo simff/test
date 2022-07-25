fab5c324cf85b7ddfd450cde5c9b8023983a89f7
Alexey Simonov <simff001@gmail.com>
2022-07-25 05:56:21 +0300

    clone VM bash script

---
+++ cloneVM-ESXi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo -------------------- Start -----------------------------
+
+ssh esxi "mkdir /vmfs/volumes/ds-01/test-nginx"
+ssh esxi "vmkfstools -i /vmfs/volumes/ds-01/ubuntu-20.04/ubuntu-20.04.vmdk /vmfs/volumes/ds-01/test-nginx/test-nginx.vmdk"
+ssh esxi "sed 's/ubuntu-20.04/test-nginx/g' /vmfs/volumes/ds-01/ubuntu-20.04/ubuntu-20.04.vmx > /vmfs/volumes/ds-01/test-nginx/test-nginx.vmx"
+idnewvm=`ssh esxi "vim-cmd solo/registervm /vmfs/volumes/ds-01/test-nginx/test-nginx.vmx"`
+echo New VM ID: $idnewvm
+ssh esxi "vim-cmd vmsvc/power.on $idnewvm &" &
+echo Power ON VM
+sleep 10
+ssh esxi "vim-cmd vmsvc/message $idnewvm" > message.vm
+idmes=`sed -n '1s/Virtual machine message//; s/://p' message.vm`
+ssh esxi "vim-cmd vmsvc/message $idmes 0 2"
+echo Answer the question and wait 5 minute
+sec=300
+                         while [ $sec -ge 0 ]; do
+                                 echo -ne "$sec\033[0K\r"
+                                 let "sec=sec-1"
+                                 sleep 1
+                         done
+
+echo Change IP address
+ssh 192.168.1.250 "ip addr add 192.168.1.155/255.255.255.0 dev ens33"
+sleep 5
+ssh 192.168.1.250 "ip addr del 192.168.1.250/255.255.255.0 dev ens33 &" &
+sleep 5
+echo Change host name
+ssh 192.168.1.155 "hostnamectl set-hostname nginx155"
+sleep 5
+echo Install Nginx
+ssh 192.168.1.155 "apt -y install nginx"
+
+echo ------------------- Finish -----------------------------
